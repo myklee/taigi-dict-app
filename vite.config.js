9db598a0a3796f36d23f6aefee8807c21f5c3a74
Mike <myklee@gmail.com>
2024-10-20 12:12:35 +0800

    gh pages deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/taigi-dict-app/',
+  base: '/taigi-dict-app/',
   plugins: [
     vue(),
   ],
